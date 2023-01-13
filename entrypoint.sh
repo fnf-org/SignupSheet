8a1c78316880110e6cdde6335fd5ee1c0855e818
Mike Matera <maximus@fatboycentral.com>
2023-01-13 15:08:33 -0800

    Take out debug stuff.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,14 @@
 
 set -e 
 
-echo STARTING Signup Application 
-
-ls -la / 
-ls -la /runtime
+echo STARTING Signup container.
 
 # Source the environment from the runtime.
 if [ -f /runtime/config ]; then 
-    echo Sourcing runtime configuration... 
+    echo Sourcing runtime /runtime/config
     cat /runtime/config
     . /runtime/config
 fi
-
-. /runtime/config
-printenv 
 
 # Apply migrations, if necessary
 echo Apply migrations...
@@ -23,11 +17,12 @@ python3 ./manage.py migrate
 
 # FIXME: Determine if initialization is needed. 
 if /bin/false; then 
+    echo "Applying default data."
     for fixture in fixtures/*.json; do 
         python3 ./manage.py loaddata $fixture
     done
 fi
 
 # Run the app
-echo Run the app!
+echo STARTING Signup application.
 python3 ./manage.py runserver --insecure 0.0.0.0:${PORT}
