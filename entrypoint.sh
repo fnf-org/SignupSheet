57bbe5d142709f8dfd6465c8cc928d967bfc7d53
Mike Matera <maximus@fatboycentral.com>
2023-01-13 15:16:03 -0800

    Simplify start. Revert management to the devs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,10 @@
 
 set -e 
 
-echo STARTING Signup container.
-
 # Source the environment from the runtime.
 if [ -f /runtime/config ]; then 
-    echo Sourcing runtime /runtime/config
     . /runtime/config
 fi
 
-# Apply migrations, if necessary
-echo Apply migrations...
-python3 ./manage.py migrate 
-
-# FIXME: Determine if initialization is needed. 
-if /bin/false; then 
-    echo "Applying default data."
-    for fixture in fixtures/*.json; do 
-        python3 ./manage.py loaddata $fixture
-    done
-fi
-
 # Run the app
-echo STARTING Signup application.
 python3 ./manage.py runserver --insecure 0.0.0.0:${PORT}
