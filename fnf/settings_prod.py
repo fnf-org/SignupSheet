e7d5e2b8ebf40cffcaefd861093456db361cfee1
Mike Matera <matera@lifealgorithmic.com>
2023-04-02 13:52:57 -0700

    Make DEBUG selectable in prod.

--- fnf/settings_prod.py
+++ fnf/settings_prod.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = False
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 ALLOWED_HOSTS = [ '*' ]
